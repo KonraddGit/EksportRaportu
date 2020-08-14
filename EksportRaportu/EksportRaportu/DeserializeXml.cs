3b7dba377a0009ba809202915d8624931bf6118f
Konrad Parchański <konradparchanski@gitlab.com>
2020-08-14 15:46:06 +0200

    deserialize xml

---
+++ EksportRaportu/EksportRaportu/DeserializeXml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace EksportRaportu
+{
+    public class DeserializeXml
+    {
+        public DeserializeXml()
+        {
+            string path = "BmwOriginal.xml";
+
+            XmlSerializer ser = new XmlSerializer(typeof(Cars));
+            Cars cars;
+
+            using (XmlReader reader = XmlReader.Create(path))
+            {
+                cars = (Cars)ser.Deserialize(reader);
+            }
+
+            Console.WriteLine(cars.Items);
+        }
+    }
+}
+
---
+++ EksportRaportu/EksportRaportu/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace EksportRaportu
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DeserializeXml deserializeXml = new DeserializeXml();
+
+            Console.ReadLine();
+        }
+    }
+}
